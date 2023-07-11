25f3577b15102a5aacd4d11a2539c5b0f1331b7b
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-07-11 06:45:38 +0300

    displaying data in the message component

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -14,27 +14,20 @@ const Message = (props: MessagePropsType) => {
             <div className={s.imageAndText}>
                 <img
                     id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
+                    src={props.message.user.avatar}
+                    alt='avatar'
                 />
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                        {props.message.user.name}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                        {props.message.message.text}
                     </pre>
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {/*создаёт студент*/}
-
-                {/**/}
+                {props.message.message.time}
             </div>
         </div>
     )
