568a286ca4bf826141264da8505a667b9cad61ce
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-07-19 06:11:38 +0300

    Styles for message component

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -11,24 +11,26 @@ export type MessagePropsType = {
 const Message = (props: MessagePropsType) => {
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
-            <div className={s.imageAndText}>
+            <div className={s.nameAndText}>
+                <div id={'hw1-name-' + props.message.id} className={s.name}>
+                    {props.message.user.name}
+                </div>
+                <div className={s.text}>
+                    {props.message.message.text}
+                </div>
+            </div>
+            <div className={s.triangle}></div>
+            <div className={s.imageAndTime}>
                 <img
                     id={'hw1-avatar-' + props.message.id}
                     src={props.message.user.avatar}
-                    alt='avatar'
+                    alt="avatar"
                 />
-                <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {props.message.user.name}
-                    </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {props.message.message.text}
-                    </pre>
+                <div id={'hw1-time-' + props.message.id} className={s.time}>
+                    {props.message.message.time}
                 </div>
             </div>
-            <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {props.message.message.time}
-            </div>
+
         </div>
     )
 }
