b908cf31a2ca89db255fefd1e5324f1c0d464ff1
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-07-19 06:24:50 +0300

    all styles completed hw1

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -9,33 +9,32 @@ const FriendMessage = (props: MessagePropsType) => {
             id={'hw1-friend-message-' + props.message.id}
             className={s.friendMessage}
         >
-            <div className={s.friendImageAndText}>
+            <div className={s.friendImageAndTime}>
                 <img
                     id={'hw1-friend-avatar-' + props.message.id}
                     src={props.message.user.avatar}
-                    alt='avatar'
+                    alt="avatar"
                 />
-                <div className={s.friendText}>
-                    <div
-                        id={'hw1-friend-name-' + props.message.id}
-                        className={s.friendName}
-                    >
-                        {props.message.user.name}
-                    </div>
-                    <pre
-                        id={'hw1-friend-text-' + props.message.id}
-                        className={s.friendMessageText}
-                    >
-                        {props.message.message.text}
-                    </pre>
+                <div
+                    id={'hw1-friend-time-' + props.message.id}
+                    className={s.friendTime}
+                >
+                    {props.message.message.time}
                 </div>
             </div>
-            <div
-                id={'hw1-friend-time-' + props.message.id}
-                className={s.friendTime}
-            >
-                {props.message.message.time}
+            <div className={s.friendTriangle}></div>
+            <div className={s.friendNameAndText}>
+                <div
+                    id={'hw1-friend-name-' + props.message.id}
+                    className={s.friendName}
+                >
+                    {props.message.user.name}
+                </div>
+                <div className={s.friendText}>
+                    {props.message.message.text}
+                </div>
             </div>
+
         </div>
     )
 }
